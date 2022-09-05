313bb5abd60ab22abefa98eb4b777b6f742f0e47
Nazmus Sakib <nazmussakib161@gmail.com>
2022-09-05 18:05:26 +0600

    second

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -9,43 +9,64 @@ import { FaUserCircle } from 'react-icons/fa';
 
 const Header = () => {
     return (
+        // <header className='header'>
+        //     <div className="container">
+        //         <nav className="navbar navbar-expand-lg">
+        //             <div className="container-fluid">
+        //                 <a className="navbar-brand" href="#">
+        //                     <img src={logo} alt="" className='img-fluid w-50' />
+        //                 </a>
+        //                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        //                     <span className="navbar-toggler-icon"></span>
+        //                 </button>
+        //                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        //                     <form className="d-flex m-auto" role="search">
+        //                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        //                         <a href="#"><BsSearch/></a>
+        //                     </form>
+        //                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        //                         <li className="nav-item">
+        //                             <a className="nav-link active" aria-current="page" href="#"><FiHome/></a>
+        //                         </li>
+        //                         <li className="nav-item">
+        //                             <a className="nav-link" href="#"><HiOutlineUserGroup/></a>
+        //                         </li>
+        //                         <li className="nav-item">
+        //                             <a className="nav-link" href="#"><FiBell/></a>
+        //                         </li>
+        //                         <li className="nav-item">
+        //                             <a className="nav-link" href="#"><AiOutlineMessage/></a>
+        //                         </li>
+        //                         <li className="nav-item">
+        //                             <a className="nav-link" href="#"><FaUserCircle/></a>
+        //                         </li>
+
+        //                     </ul>
+
+        //                 </div>
+        //             </div>
+        //         </nav>
+        //     </div>
+        // </header>
+
         <header className='header'>
             <div className="container">
-                <nav className="navbar navbar-expand-lg">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">
-                            <img src={logo} alt="" className='img-fluid w-50' />
-                        </a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <form className="d-flex m-auto" role="search">
-                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                <a href="#"><BsSearch/></a>
-                            </form>
-                            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#"><FiHome/></a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#"><HiOutlineUserGroup/></a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#"><FiBell/></a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#"><AiOutlineMessage/></a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#"><FaUserCircle/></a>
-                                </li>
-
-                            </ul>
-
-                        </div>
+                <div className="navbar">
+                    <div className="logo"><img src={logo} alt="" width='50px' /></div>
+                    <div className="nav-search">
+                        <form className="d-flex m-auto" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <a href="#"><BsSearch /></a>
+                        </form>
                     </div>
-                </nav>
+                    <ul className="nav-menu">
+                        <li className="nav-menu-item"><a href="#"><FiHome /></a></li>
+                        <li className="nav-menu-item"><a href="#"><HiOutlineUserGroup /></a></li>
+                        <li className="nav-menu-item"><a href="#"><FiBell /></a></li>
+                        <li className="nav-menu-item"><a href="#"><AiOutlineMessage /></a></li>
+                        <li className="nav-menu-item"><a href="#"><FaUserCircle /></a></li>
+                    </ul>
+                </div>
             </div>
         </header>
     );
